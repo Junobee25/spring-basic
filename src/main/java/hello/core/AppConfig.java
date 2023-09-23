877e5a9d875edfd7b1630706050cb2ef1158608b
Junobee25 <wnsdhqo@naver.com>
2023-09-23 23:36:59 +0900

    Feat : appconfig 에서 spring으로의 전환

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -8,22 +8,27 @@ import hello.core.member.MemberServiceimpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
+
+    @Bean
     public MemberService memberService() {
         // 생성자 주입
         return new MemberServiceimpl(memberRepository());
     }
-
-    private MemoryMemberRepository memberRepository(){
+    @Bean
+    public MemoryMemberRepository memberRepository(){
         return new MemoryMemberRepository();
     }
-
+    @Bean
     public OrderService orderService() {
         // 생성자 주입
         return new OrderServiceImpl(memberRepository(), disCountPolicy());
     }
-
+    @Bean
     public DisCountPolicy disCountPolicy() {
         // 할인 정책 역할을 담당하는 구현 (객체 변경하면 끝)
         return new RateDisCountPolicy();
