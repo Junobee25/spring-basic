908791e3cabcf82ed714a0f9cf41d46a4e7b88ad
Junobee25 <wnsdhqo@naver.com>
2023-09-23 21:51:54 +0900

    Refactor : Appconfig

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,5 +1,6 @@
 package hello.core;
 
+import hello.core.discount.DisCountPolicy;
 import hello.core.discount.FixDisCountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceimpl;
@@ -10,11 +11,19 @@ import hello.core.order.OrderServiceImpl;
 public class AppConfig {
     public MemberService memberService() {
         // 생성자 주입
-        return new MemberServiceimpl(new MemoryMemberRepository());
+        return new MemberServiceimpl(memberRepository());
+    }
+
+    private MemoryMemberRepository memberRepository(){
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
         // 생성자 주입
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDisCountPolicy());
+        return new OrderServiceImpl(memberRepository(), disCountPolicy());
+    }
+
+    public DisCountPolicy disCountPolicy() {
+        return new FixDisCountPolicy();
     }
 }
