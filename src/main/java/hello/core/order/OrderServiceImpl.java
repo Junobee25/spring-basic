cc561e5f3f3dbcbcdfe71391fc908ba920490cb9
Junobee25 <wnsdhqo@naver.com>
2023-09-23 19:44:36 +0900

    Feat : ratediscountpolicy

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.order;
 
 import hello.core.discount.DisCountPolicy;
 import hello.core.discount.FixDisCountPolicy;
+import hello.core.discount.RateDisCountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -9,7 +10,8 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DisCountPolicy disCountPolicy = new FixDisCountPolicy();
+//    private final DisCountPolicy disCountPolicy = new FixDisCountPolicy();
+    private final DisCountPolicy disCountPolicy = new RateDisCountPolicy();
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
