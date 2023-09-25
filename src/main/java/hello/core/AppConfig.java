c5efc0df7fc29fc34e430d4b80b0ef572aa73835
Junobee25 <wnsdhqo@naver.com>
2023-09-26 01:32:12 +0900

    Design : spring container singleton call

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -3,6 +3,7 @@ package hello.core;
 import hello.core.discount.DisCountPolicy;
 import hello.core.discount.FixDisCountPolicy;
 import hello.core.discount.RateDisCountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceimpl;
 import hello.core.member.MemoryMemberRepository;
@@ -16,15 +17,18 @@ public class AppConfig {
 
     @Bean
     public MemberService memberService() {
+        System.out.println("Call AppConfig.memberService");
         // 생성자 주입
         return new MemberServiceimpl(memberRepository());
     }
     @Bean
     public MemoryMemberRepository memberRepository(){
+        System.out.println("Call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
     @Bean
     public OrderService orderService() {
+        System.out.println("Call AppConfig.orderService");
         // 생성자 주입
         return new OrderServiceImpl(memberRepository(), disCountPolicy());
     }
@@ -33,4 +37,6 @@ public class AppConfig {
         // 할인 정책 역할을 담당하는 구현 (객체 변경하면 끝)
         return new RateDisCountPolicy();
     }
+
+
 }
