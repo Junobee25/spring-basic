d0f766508ecf3eb288dfd7033194c9b1552a4852
Junobee25 <wnsdhqo@naver.com>
2023-09-28 23:49:27 +0900

    Component : 탐색 위치와 기본 스캔 대상

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -7,7 +7,9 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
-        // 탐색 시작 위치. core만 컴포넌트 스캔의 대상이 됨
+        // 탐색 시작 위치. core만 컴포넌트 스캔의 대상이 됨 다른 경로 안 찾아도 되니 효율적임
+        // default는 ComponentScan 넣어준 class의 패키지 위치 => hello.core
+        // 보통 autoconfig, appconfig같은 설정 정보 클래스의 위치를 프로젝트 최상단에 두면 basePackages 추가할 필요 없음.
         basePackages = "hello.core",
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
